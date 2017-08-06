047d7f0e4198bb5795117c4bff015119d09e2830
sablaireau <costa1.hope@libero.it>
2017-08-06 23:38:59 +0200

    Dinamic complile.sh

--- compile.sh
+++ compile.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-echo "riconfigurazione di HELLO";
+configStr="DUMBVM"
+echo "riconfigurazione di $configStr" ;
 cd /home/pds/os161/os161-base-2.0.2/kern/conf;
-./config HELLO;
-cd /home/pds/os161/os161-base-2.0.2/kern/compile/HELLO;
+./config $configStr;
+cd /home/pds/os161/os161-base-2.0.2/kern/compile/$configStr;
 bmake depend;
 bmake;
 bmake install;
 cd /home/pds/pds-os161/root;
 sys161 kernel;
-
-
